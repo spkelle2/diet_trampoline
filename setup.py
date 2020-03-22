98772349537f6436e244775f820d8c84299d743d
Sean Kelley <seanpkelley94@gmail.com>
2020-03-22 12:32:08 -0500

    adjusted setup.py to include subpackages

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/spkelle2/diet",
-    packages=['diet', 'test_diet'],
+    packages=['diet', 'diet.schemas', 'diet.solve', 'test_diet'],
     install_requires=[
         'ticdat',
     ],
